8dafd31f751771b0b3861a6bc1467e535c8232e7
solsol0609 <solsol0609@naver.com>
2019-05-27 15:47:07 +0900

    퀴즈 2 문제 추가

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -14,20 +14,32 @@
         {var val ="가스하이드레이트"
         if(val == (document.getElementById("print5").value))
 
+            {var val ="이사부"
+            if(val == (document.getElementById("print6").value))
+
+                {var val ="삼봉도"
+                if(val == (document.getElementById("print7").value))
+
             {alert("열심히 공부했군요? 모두 다 맞혔습니다! 대단합니다!");
             window.open('../wow/index.html');
             }
-                else{alert("5번이 오답입니다.");}
+
+            else{alert("7번이 오답입니다.");}
+            }
+            
+            else{alert("6,7번이 오답입니다.");}
+            }
+                else{alert("5,6,7번이 오답입니다.");}
                 }
 
-                    else{alert("4,5번이 오답입니다.");}
+                    else{alert("4,5,6,7번이 오답입니다.");}
                     }
 
-                        else{alert("3,4,5번이 오답입니다.");}
+                        else{alert("3,4,5,6,7번이 오답입니다.");}
                             }
 
-        	                    else{alert("2,3,4,5번 중 오답이 있습니다.");}
+        	                    else{alert("2,3,4,5,6,7번 중 오답이 있습니다.");}
                                 }
                                 
-                                    else{alert("1,2,3,4,5번 중 오답이 있습니다.");}
+                                    else{alert("1,2,3,4,5,6,7번 중 오답이 있습니다.");}
     	                            }
